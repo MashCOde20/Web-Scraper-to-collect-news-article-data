7d67b2e1c0124504d96c961772b89a77cbe62412
Matthew <88686556+MashCOde20@users.noreply.github.com>
2024-10-15 15:24:16 -0400

    Update web_scraperCVD19Data.py
    Gets COVID-19 articles from New York times

--- web_scraperCVD19Data.py
+++ web_scraperCVD19Data.py
@@ -1,27 +1,34 @@
-#Project scrapes data on ny times site and return title and body of article
+# Project scrapes data on NY Times site and returns title and body of the article
 
-#Imports beautiful soup and request library
-import contextlib
+# Imports beautiful soup and requests library
 import requests
-from bs4  import BeautifulSoup
+from bs4 import BeautifulSoup
 
-#Stores URL in url variable
-site_url ='https://www.nytimes.com/2020/09/02/opinion/remote-learning-coronavirus.html'
+# Stores URL in site_url variable
+site_url = 'https://www.nytimes.com/2020/09/02/opinion/remote-learning-coronavirus.html'
 
+# Get request of site
+response = requests.get(site_url)
 
+# Check if the request was successful
+if response.status_code == 200:
+    data = response.text
 
+    # Parse the HTML content using BeautifulSoup
+    soup = BeautifulSoup(data, "html.parser")
 
-#get request of site
+    # Extract title
+    title = soup.find(name="h1")
+    if title:
+        print("Title:", title.get_text())
+    else:
+        print("Title not found.")
 
-response = requests.get(url=site_url)
-data = response.text
-
-
-
-
-soup = BeautifulSoup(data, "html.parser")
-soup.prettify()
-title = soup.find(name="h1", id="main-content")
-print(title.getText())
-print(contextlib.getText())
-
+    # Extract article body
+    paragraphs = soup.find_all('p')
+    body = ' '.join([para.get_text() for para in paragraphs])
+    
+    # Print the body of the article
+    print("Body:", body)
+else:
+    print("Failed to retrieve the page. Status code:", response.status_code)
